065ba2385d08e24c3df14ad7125afec1bfcd636f
zahram456 <zaharamuhammad456@gmail.com>
2026-02-16 09:10:10 +0500

    Remove extra launcher and path files

--- TARZAN.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Users\abuzain\AppData\Local\Programs\Python\Python313\python.exe" "d:\My projects\TarzanGame - Copy\main.py"
-pause
--- run.ps1
+++
@@ -1,1 +0,0 @@
-& "C:\Users\abuzain\AppData\Local\Programs\Python\Python313\python.exe" "d:/My projects/TarzanGame - Copy/main.py"
